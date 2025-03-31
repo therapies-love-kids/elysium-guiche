b4a5ec2e9cb1d074a4b849efcbafced9c958d8d9
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-31 13:48:21 +0000

    MOD: Feito a troca de controlador para </App >

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Root from './root.tsx'
+import App from './App.tsx'
 
 const rootElement = document.getElementById('root') as HTMLElement
 createRoot(rootElement).render(
   <StrictMode>
-    <Root />
+    <App />
   </StrictMode>
 )
