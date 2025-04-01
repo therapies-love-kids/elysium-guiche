a2008b3b12465f726546a7f3e6d2947cc4c83eb0
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-01 12:44:33 +0000

    ADD: Tabelas de referência adicionadas e restrições de chave estrangeira atualizadas no esquema do banco de dados

--- docs/sql/ddl.sql
+++ docs/sql/ddl.sql
@@ -1,4 +1,61 @@
--- Criar tabelas primeiro e relações depois
+-- Tabela de referência para unidade_prefixo (comum a várias tabelas)
+CREATE TABLE UNIDADE_PREFIXO (
+    pk SERIAL PRIMARY KEY,
+    prefixo VARCHAR(4) UNIQUE NOT NULL,
+    descricao VARCHAR(32) NOT NULL
+);
+
+-- Referências para AGENDAMENTO
+CREATE TABLE AGENDAMENTO_SALA (
+    pk SERIAL PRIMARY KEY,
+    sala VARCHAR(16) UNIQUE NOT NULL,
+    descricao VARCHAR(64)
+);
+
+CREATE TABLE AGENDAMENTO_TIPO (
+    pk SERIAL PRIMARY KEY,
+    tipo VARCHAR(64) UNIQUE NOT NULL,
+    descricao VARCHAR(128)
+);
+
+CREATE TABLE AGENDAMENTO_STATUS (
+    pk SERIAL PRIMARY KEY,
+    status VARCHAR(16) UNIQUE NOT NULL,
+    descricao VARCHAR(64)
+);
+
+-- Referências para COLABORADOR
+CREATE TABLE COLABORADOR_SETOR (
+    pk SERIAL PRIMARY KEY,
+    setor VARCHAR(64) UNIQUE NOT NULL,
+    descricao VARCHAR(128)
+);
+
+CREATE TABLE COLABORADOR_FUNCAO (
+    pk SERIAL PRIMARY KEY,
+    funcao VARCHAR(64) UNIQUE NOT NULL,
+    descricao VARCHAR(128)
+);
+
+CREATE TABLE COLABORADOR_ESPECIALIDADE (
+    pk SERIAL PRIMARY KEY,
+    especialidade VARCHAR(64) UNIQUE NOT NULL,
+    descricao VARCHAR(128)
+);
+
+-- Referências para DOCUMENTO
+CREATE TABLE DOCUMENTO_TIPO (
+    pk SERIAL PRIMARY KEY,
+    tipo VARCHAR(64) UNIQUE NOT NULL,
+    descricao VARCHAR(128)
+);
+
+-- Referências para USUARIO
+CREATE TABLE USUARIO_PERFIL (
+    pk SERIAL PRIMARY KEY,
+    perfil VARCHAR(16) UNIQUE NOT NULL,
+    descricao VARCHAR(64)
+);
 
 CREATE TABLE EMPRESA (
     pk SERIAL PRIMARY KEY,
@@ -15,21 +72,14 @@ CREATE TABLE EMPRESA (
     logradouro VARCHAR(128) NOT NULL,
     numero VARCHAR(8) NOT NULL,
     complemento VARCHAR(256),
-    unidade_prefixo VARCHAR(4) NOT NULL UNIQUE,
+    unidade_prefixo_pk INTEGER NOT NULL REFERENCES UNIDADE_PREFIXO(pk),
     unidade_nome VARCHAR(32) UNIQUE NOT NULL
-);
-
-CREATE TABLE CONVENIO (
-    pk SERIAL PRIMARY KEY,
-    ativo BOOLEAN NOT NULL,
-    nome VARCHAR(64) NOT NULL,
-    nome_curto VARCHAR(16) NOT NULL
 );
 
 CREATE TABLE COLABORADOR (
     pk SERIAL PRIMARY KEY,
     id SERIAL UNIQUE NOT NULL,
-    empresa_unidade_prefixo VARCHAR NOT NULL REFERENCES EMPRESA(unidade_prefixo),
+    empresa_unidade_prefixo_pk INTEGER NOT NULL REFERENCES UNIDADE_PREFIXO(pk),
     ativo BOOLEAN NOT NULL,
     nome VARCHAR(128) NOT NULL,
     sexo CHAR(1) NOT NULL,
@@ -58,9 +108,9 @@ CREATE TABLE COLABORADOR (
     data_admissao DATE NOT NULL,
     data_demissao DATE,
     pis VARCHAR(16) UNIQUE,
-    setor VARCHAR(64) NOT NULL,
-    funcao VARCHAR(64) NOT NULL,
-    especialidade VARCHAR(64)
+    setor_pk INTEGER NOT NULL REFERENCES COLABORADOR_SETOR(pk),
+    funcao_pk INTEGER NOT NULL REFERENCES COLABORADOR_FUNCAO(pk),
+    especialidade_pk INTEGER REFERENCES COLABORADOR_ESPECIALIDADE(pk)
 );
 
 CREATE TABLE USUARIO (
@@ -71,19 +121,13 @@ CREATE TABLE USUARIO (
     nome VARCHAR(64) NOT NULL,
     senha VARCHAR(128) NOT NULL,
     nome_computador VARCHAR(64) NOT NULL,
-    perfil VARCHAR(16) NOT NULL
+    perfil_pk INTEGER NOT NULL REFERENCES USUARIO_PERFIL(pk)
 );
-
--- Adiciona a constraint de chave estrangeira para diretor_colaborador_id após a criação da tabela COLABORADOR
-ALTER TABLE EMPRESA
-ADD CONSTRAINT fk_diretor_colaborador
-FOREIGN KEY (diretor_colaborador_id)
-REFERENCES COLABORADOR(id);
 
 CREATE TABLE PACIENTE (
     pk SERIAL PRIMARY KEY,
     id SERIAL UNIQUE NOT NULL,
-    empresa_unidade_prefixo VARCHAR NOT NULL REFERENCES EMPRESA(unidade_prefixo),
+    empresa_unidade_prefixo_pk INTEGER NOT NULL REFERENCES UNIDADE_PREFIXO(pk),
     convenio_pk INTEGER NOT NULL REFERENCES CONVENIO(pk),
     codigo VARCHAR(64) UNIQUE NOT NULL,
     ativo BOOLEAN NOT NULL,
@@ -98,50 +142,19 @@ CREATE TABLE PACIENTE (
     observacoes TEXT
 );
 
-CREATE TABLE RESPONSAVEL (
-    pk SERIAL PRIMARY KEY,
-    id SERIAL UNIQUE NOT NULL,
-    ativo BOOLEAN NOT NULL,
-    nome VARCHAR(128) NOT NULL,
-    estado_civil VARCHAR(16) NOT NULL,
-    profissao VARCHAR(64) NOT NULL,
-    cpf VARCHAR(11) UNIQUE NOT NULL,
-    rg VARCHAR(20) UNIQUE NOT NULL,
-    celular VARCHAR(16) UNIQUE NOT NULL,
-    email VARCHAR(64) UNIQUE NOT NULL,
-    contatos_extras TEXT
-);
-
-CREATE TABLE LACO (
-    pk SERIAL PRIMARY KEY,
-    paciente_id INTEGER NOT NULL REFERENCES PACIENTE(id),
-    responsavel_id INTEGER NOT NULL REFERENCES RESPONSAVEL(id),
-    data_hora_criacao TIMESTAMPTZ NOT NULL,
-    tipo VARCHAR(16) NOT NULL
-);
-
 CREATE TABLE AGENDAMENTO (
     pk SERIAL PRIMARY KEY,
     especialista_colaborador_id INTEGER NOT NULL REFERENCES COLABORADOR(id),
     paciente_id INTEGER NOT NULL REFERENCES PACIENTE(id),
     recepcionista_colaborador_id INTEGER REFERENCES COLABORADOR(id),
     responsavel_id INTEGER REFERENCES RESPONSAVEL(id),
-    unidade_prefixo VARCHAR NOT NULL REFERENCES EMPRESA(unidade_prefixo),
+    unidade_prefixo_pk INTEGER NOT NULL REFERENCES UNIDADE_PREFIXO(pk),
     data_hora_criacao TIMESTAMPTZ NOT NULL,
     data_hora TIMESTAMPTZ NOT NULL,
-    sala VARCHAR(16) NOT NULL,
-    tipo VARCHAR(64) NOT NULL,
-    "status" VARCHAR(16) NOT NULL DEFAULT 'Agendado',
+    sala_pk INTEGER NOT NULL REFERENCES AGENDAMENTO_SALA(pk),
+    tipo_pk INTEGER NOT NULL REFERENCES AGENDAMENTO_TIPO(pk),
+    status_pk INTEGER NOT NULL REFERENCES AGENDAMENTO_STATUS(pk), -- Supondo que 'Agendado' seja o valor padrão
     observacoes TEXT
-);
-
-CREATE TABLE PAGAMENTO (
-    pk SERIAL PRIMARY KEY,
-    responsavel_id INTEGER NOT NULL REFERENCES RESPONSAVEL(id),
-    data_hora_criacao TIMESTAMPTZ NOT NULL,
-    valor NUMERIC(10, 2) NOT NULL,
-    tipo VARCHAR(64) NOT NULL,
-    descricao TEXT
 );
 
 CREATE TABLE DOCUMENTO (
@@ -150,7 +163,7 @@ CREATE TABLE DOCUMENTO (
     colaborador_id INTEGER NOT NULL REFERENCES COLABORADOR(id),
     ativo BOOLEAN NOT NULL,
     data_hora_criacao TIMESTAMPTZ NOT NULL,
-    tipo VARCHAR(64) NOT NULL,
+    tipo_pk INTEGER NOT NULL REFERENCES DOCUMENTO_TIPO(pk),
     caminho VARCHAR(256) NOT NULL
 );
 
@@ -179,3 +192,46 @@ CREATE TABLE CAMINHO (
     editavel BOOLEAN NOT NULL,
     leitura BOOLEAN NOT NULL
 );
+
+CREATE TABLE PAGAMENTO (
+    pk SERIAL PRIMARY KEY,
+    responsavel_id INTEGER NOT NULL REFERENCES RESPONSAVEL(id),
+    data_hora_criacao TIMESTAMPTZ NOT NULL,
+    valor NUMERIC(10, 2) NOT NULL,
+    tipo VARCHAR(64) NOT NULL,
+    descricao TEXT
+);
+
+CREATE TABLE LACO (
+    pk SERIAL PRIMARY KEY,
+    paciente_id INTEGER NOT NULL REFERENCES PACIENTE(id),
+    responsavel_id INTEGER NOT NULL REFERENCES RESPONSAVEL(id),
+    data_hora_criacao TIMESTAMPTZ NOT NULL,
+    tipo VARCHAR(16) NOT NULL
+);
+
+CREATE TABLE RESPONSAVEL (
+    pk SERIAL PRIMARY KEY,
+    id SERIAL UNIQUE NOT NULL,
+    ativo BOOLEAN NOT NULL,
+    nome VARCHAR(128) NOT NULL,
+    estado_civil VARCHAR(16) NOT NULL,
+    profissao VARCHAR(64) NOT NULL,
+    cpf VARCHAR(11) UNIQUE NOT NULL,
+    rg VARCHAR(20) UNIQUE NOT NULL,
+    celular VARCHAR(16) UNIQUE NOT NULL,
+    email VARCHAR(64) UNIQUE NOT NULL,
+    contatos_extras TEXT
+);
+
+CREATE TABLE CONVENIO (
+    pk SERIAL PRIMARY KEY,
+    ativo BOOLEAN NOT NULL,
+    nome VARCHAR(64) NOT NULL,
+    nome_curto VARCHAR(16) NOT NULL
+);
+
+ALTER TABLE EMPRESA
+ADD CONSTRAINT fk_diretor_colaborador
+FOREIGN KEY (diretor_colaborador_id)
+REFERENCES COLABORADOR(id);
