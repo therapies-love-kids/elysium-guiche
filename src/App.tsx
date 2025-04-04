970cd9d2aea6ac601829c60dd301d6f43df45fac
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-04 16:21:57 +0000

    ADD: Novas rotas adicionadas FIX: Correção de erro de digitação no caminho de importação de recepcionist

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ import Guiche from "./pages/Guiche";
 import Telaatendimento from "./pages/Profile/medic";
 import Help from "./pages/helpPassword";
 import Bloco from "./pages/Profile/everyone/bloco";
-import Recepcionist from "./pages/Profile/receptionist";
+import Recepcionist from "./pages/Profile/recepcionist";
+import Addconvenio from "./pages/Profile/recepcionist/addconvenio";
+import Addmedic from "./pages/Profile/recepcionist/addmedic";
+import Addpatient from "./pages/Profile/recepcionist/addpatient";
 
 function App() {
   return (
@@ -22,6 +25,9 @@ function App() {
         <Route path="/helpPassword" element={<Help />} />
         <Route path="/bloco" element={<Bloco />} />
         <Route path="/recepcionist" element={<Recepcionist />} />
+        <Route path="/addconvenio" element={<Addconvenio />} />
+        <Route path="/addmedic" element={<Addmedic />} />
+        <Route path="/addpatient" element={<Addpatient />} />
       </Routes>
     </Router>
   );
